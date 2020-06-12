64ce857b8009f611c77f5648e5d1ece1e30fabd5
tyler broadway <tbroadway96@gmail.com>
2020-06-11 22:40:57 -0500

    Project complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,59 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    const succeeded = {
+      name: item.name, 
+      enhancement: item.enhancement + 1,
+      durability: item.durability
+    }
+    return succeeded;
+  } else {
+    const didNotSucceed = {
+      name: item.name, 
+      enhancement: item.enhancement, 
+      durability: item.durability
+    }
+    return didNotSucceed;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    const failed = {
+      name: item.name,
+      enhancement: item.enhancement,
+      durability: item.durability - 5
+    }
+    return failed
+
+  } else if (item.enhancement > 15) {
+    const failed = {
+      name: item.name,
+      enhancement: item.enhancement,
+      durability: item.durability - 10
+    }
+    return failed
+
+  } else if (item.enhancement > 16) {
+    const failed = {
+      name: item.name,
+      enhancement: item.enhancement - 1,
+      durability: item.durability
+    }
+    return failed;
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  if (item.durability === 0 || item.durability < 101) {
+    const repaired = {
+      name: item.name,
+      enhancement: item.enhancement,
+      durability: item.durability = 100
+    }
+    return repaired;
+  }
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,87 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('item functions', () => {
+    describe('succeed test', () => {
+      it('should enhance item by 1', () => {
+  
+        const item = {
+          name: "succeed",
+          enhancement: 18,
+          durability: 100
+        };
+  
+        const expected = {
+          name: "succeed",
+          enhancement: 19,
+          durability: 100
+        };
+  
+        const succeeded = enhancer.succeed(item);
+
+        expect(succeeded).toStrictEqual(expected);
+  
+      });
+    });
+
+    describe('fail test', () => {
+        it('should decrease durability by 5', () => {
+    
+          const lessThan = {
+            name: "fail",
+            enhancement: 10,
+            durability: 80
+          };
+    
+          const expectedFail = {
+            name: "fail",
+            enhancement: 10,
+            durability: 75
+          };
+    
+          const failItem = enhancer.fail(lessThan);
+    
+          expect(failItem).toStrictEqual(expectedFail);
+        })
+    
+        it("should decrease durability by 10", () => {
+    
+          const lessThan = {
+            name: "fail",
+            enhancement: 20,
+            durability: 80
+          };
+    
+          const expectedFail = {
+            name: "fail",
+            enhancement: 20,
+            durability: 70
+          };
+    
+          const failItem = enhancer.fail(lessThan);
+    
+          expect(failItem).toStrictEqual(expectedFail);
+        });
+      });
+
+      describe('repair item', () => {
+        it('should repair durability to 100', () => {
+    
+          const repairItem = {
+            name: 'repair',
+            enhancement: 19,
+            durability: 40
+          };
+    
+          const expectedRepair = {
+            name: 'repair',
+            enhancement: 19,
+            durability: 100
+          };
+    
+          const duraRepair = enhancer.repair(repairItem);
+    
+          expect(duraRepair).toStrictEqual(expectedRepair);
+        })
+      });
+});
